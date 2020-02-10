ba89e299064a12b4073d5609e104ce665255058b
Melissa Preece <melissanchez@gmail.com>
2020-02-09 18:21:57 -0800

    Built and tested getRandomPhrase() method in Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Life is like a box of chocolates'),
+            new Phrase('Say hello to my little friend'),
+            new Phrase('Show me the money'),
+            new Phrase('May the force be with you'),
+            new Phrase('I see dead people')
+        ];
+        this.activePhrase = null;
+    }
+
+    /***
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    ***/
+    getRandomPhrase() {
+        let i = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[i];  
+    };
+}
+
+//NEXT TO DO: SEE STEP 6 IN PROJECT GUIDE
--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//TEST CODE (PLEASE DELETE BEFORE FINAL SUBMIT)
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase-phrase: ${phrase.phrase}`);
+
+// const game = new Game();
+// game.phrases.forEach((phrase, index) => {
+// console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+// });
+
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+
+// const game = new Game();
+
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+// logPhrase(game.getRandomPhrase());
+
